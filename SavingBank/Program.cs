9d7662aed51f3bb90a4c99cad3f67e161e6be87d
a4ashish8 <92498926+a4ashish8@users.noreply.github.com>
2023-02-06 18:11:29 +0530

    Update Program.cs

--- SavingBank/Program.cs
+++ SavingBank/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,56 +17,20 @@ namespace SavingBank
             { 
                 case 1:
                     Console.WriteLine("Welcome to your current bank account section");
-                    CurrentAccountClass obj = new CurrentAccountClass();
-                    Console.WriteLine("enter the Banking type  \n 1. For deposit \n 2. for withdraw \n 3. for balance check");
-                    int b = int.Parse(Console.ReadLine());
-                    switch (b)
-                    {
-                        case 1:
-                            obj.Deposit_ammount(123);
-                            break;
-
-                        case 2:
-                            obj.WithDrawl_Ammount(87);
-                            break;
-
-                        case 3:
-                            obj.Check_Ammount();
-                            Console.WriteLine("your total balance is: {0}", obj.Check_Ammount());
-                            break;
-
-                        default:
-                            Console.WriteLine("enter a valid no.");
-                            break;
-                    }
+                    CurrentAccountClass current = new CurrentAccountClass();
+                    current.Deposit_ammount(1234);
+                    current.WithDrawl_Ammount(34);
+                    current.Check_Ammount();
                     break;
 
                 case 2:
                     Console.WriteLine("Welcome to your Saving bank account section");
-                    CurrentAccountClass save = new CurrentAccountClass();
-                    Console.WriteLine("enter the Banking type  \n 1. For deposit \n 2. for withdraw \n 3. for balance check");
-                    int c = int.Parse(Console.ReadLine());
-                    switch (c)
-                    {
-                        case 1:
-                            save.Deposit_ammount(123);
-                            break;
+                    SavingAccount save = new SavingAccount();
+                    save.Deposit_ammount(11222);
+                    save.WithDrawl_Ammount(23);
+                    save.Check_Ammount();
+                    break;
 
-                        case 2:
-                            save.WithDrawl_Ammount(87);
-                            break;
-
-                        case 3:
-                            save.Check_Ammount();
-                            Console.WriteLine("your total balance is: {0}", save.Check_Ammount());
-                            break;
-
-                        default:
-                            Console.WriteLine("enter a valid no.");
-                            break;
-
-                    }
-                    break; 
 
                     default: 
                               Console.WriteLine("enter a valid no. ");
